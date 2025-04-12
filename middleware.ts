b78a696f2005496c52eb5de66599885dbb461c28
fchandiac <fchandiac@github.com>
2025-04-12 16:23:31 -0400

    TryAgain02

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,17 @@ import { getToken } from 'next-auth/jwt';
 export async function middleware(req: NextRequest) {
   const token = await getToken({
     req,
-    secret: process.env.NEXTAUTH_SECRET || 'default_secret',
+    secret: process.env.NEXTAUTH_SECRET,
   });
+
+  console.log('Token:', token);
+  console.log('Request URL:', req.url);
+  console.log('Request Body:', req.body);
+  console.log('Request Cookies:', req.cookies);
+  console.log('Request NextUrl:', req.nextUrl);
+  console.log('AuthSecret', process.env.NEXTAUTH_SECRET);
+  console.log('AuthUrl', process.env.NEXTAUTH_URL);
+
 
   // Si el usuario no está autenticado, redirige al login (/)
   if (!token) {
